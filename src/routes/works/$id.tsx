4e2d7c2bdeec113a3f4d1fb323f5e9566ad0d2f9
Angel-Dijoux <angel.dijoux@yahoo.com>
2024-04-01 18:52:17 +0200

    create markdwon wrapper

--- src/routes/works/$id.tsx
+++ src/routes/works/$id.tsx
@@ -9,6 +9,8 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Suspense } from "react";
 import useSWR from "swr";
 import { Project } from "./index.lazy";
+import { ButtonWithIcon } from "@/components/ui/ButtonWithIcon";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export const Route = createFileRoute("/works/$id")({
   component: WorkDetails,
@@ -18,11 +20,13 @@ function WorkDetails() {
   const { id } = Route.useParams();
   const { data: project, isLoading } = useSWR<Project>(`/project/${id}`);
 
+  console.log(project);
+
   if (isLoading) return <div>Loading...</div>;
   return (
     <AnimatedLayout title={project?.title}>
       <Typography variant="h3" className="my-4">
-        <MarkdownWrapper content={project?.title} />
+        <MarkdownWrapper content={project?.title} textOnly={false} />
       </Typography>
       <Suspense fallback={<SkeletonImage />}>
         {project?.image_url ? (
@@ -32,16 +36,22 @@ function WorkDetails() {
           />
         ) : null}
       </Suspense>
-      {project?.labels?.map((label) => {
-        return <Badge key={label.id}>{label.label}</Badge>;
-      })}
+      <div className="flex  gap-2">
+        {project?.labels?.map((label) => {
+          return <Badge key={label.id}>{label.label}</Badge>;
+        })}
+      </div>
       <div className="flex flex-row items-center gap-3 mt-4">
         <a href={project?.github_url} target="_blank">
-          <Button>Voir</Button>
+          <ButtonWithIcon
+            IconLeft={<GitHubLogoIcon width={ICON_SIZE} height={ICON_SIZE} />}
+          >
+            View on GitHub
+          </ButtonWithIcon>
         </a>
         {project?.project_url ? (
           <a href={project.project_url} target="_blank">
-            <Button>View</Button>
+            <Button>View project</Button>
           </a>
         ) : null}
       </div>
@@ -51,3 +61,5 @@ function WorkDetails() {
     </AnimatedLayout>
   );
 }
+
+const ICON_SIZE = 18;
