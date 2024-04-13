7e9b305ae0f8f0dd98cc93cbaeebfdb3fca45d3d
Angel-Dijoux <angel.dijoux@yahoo.com>
2024-04-13 16:21:08 +0200

    feat: adapt images

--- front_vite/src/components/layouts/AnimatedLayout.tsx
+++ front_vite/src/components/layouts/AnimatedLayout.tsx
@@ -10,11 +10,13 @@ const variants: Variants = {
 
 type LayoutProps = {
   title?: string;
+  imgSrc?: string;
 };
 
 export const AnimatedLayout = ({
   children,
   title,
+  imgSrc,
 }: PropsWithChildren<LayoutProps>) => {
   const t = title ? `${title} - Angel Dijoux` : "Angel Dijoux";
   return (
@@ -26,13 +28,15 @@ export const AnimatedLayout = ({
       transition={{ duration: 0.4, type: "easeInOut" }}
       style={{ position: "relative", marginBottom: 24 }}
     >
-      {title && (
+      {title ? (
         <Helmet>
           <title>{t}</title>
           <meta name="twitter:title" content={t} />
           <meta property="og:title" content={t} />
+          {imgSrc ? <meta name="twitter:image" content={imgSrc} /> : null}
+          {imgSrc ? <meta property="og:image" content={imgSrc} /> : null}
         </Helmet>
-      )}
+      ) : null}
       {children}
     </motion.article>
   );
--- front_vite/src/components/skeletons/SkeletonImage.tsx
+++ front_vite/src/components/skeletons/SkeletonImage.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 export function SkeletonImage() {
   return (
     <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[140px] w-[250px] rounded-xl" />
+      <Skeleton className="h-full w-full rounded-xl" />
     </div>
   );
 }
